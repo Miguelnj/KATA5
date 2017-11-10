669aee7ed74c875a911fc8320d5222f74dd16ff7
Miguel Navarro <miguel.navjor@gmail.com>
2017-11-10 12:42:58 +0100

    Now we also create a new table named "Nueva".

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
 import java.sql.*;
 
 
+
 public class Main {
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
         Class.forName("org.sqlite.JDBC");
         Connection connection = DriverManager.getConnection("jdbc:sqlite:KATA5.db");
-
         Statement statement = connection.createStatement();
 
+        queryPeople(statement);
+        createTable(statement);
+
+    }
+
+    private static void createTable(Statement statement) throws SQLException {
+        statement.execute("CREATE TABLE IF NOT EXISTS 'NUEVA' ('ID' INTEGER PRIMARY " +
+                "KEY AUTOINCREMENT," + "'MAIL' TEXT NOT NULL);");
+
+    }
+
+    private static void queryPeople(Statement statement) throws SQLException {
         ResultSet resultSet = statement.executeQuery("SELECT * FROM PEOPLE;");
 
         while(resultSet.next()) {
             System.out.print("ID = " + resultSet.getInt("ID") + "\t");
             System.out.println("NAME = " + resultSet.getString("Name"));
         }
+
     }
+
 }
